958f5d024d2f5696c186754e97eada2e13777a72
Daemon Smith <jaquallfulmore@gmail.com>
2024-11-02 18:01:35 -0400

    del another file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./Home";
 import Forms from "./compontents/Forms";
 import NewUser from "./NewUser";
 import Navbar from "./compontents/Navbar";
-import Header from "./Header";
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
       <div className=' h-fit w-full bg-gradient-to-br from-zinc-950 via-zinc-950 to-stone-900 p-8 pb-14'>
         <Router>
           <Navbar />
-          <Header />
+
           <Routes>
             <Route key={1} path='/:username' element={<Home />} />
             <Route key={2} path='/info' element={<Forms></Forms>}></Route>
